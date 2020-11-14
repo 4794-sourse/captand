fc8a9a91b2d152a08973a5351147c9fa4556eee0
4794-sourse <2456170006@qq.com>
2020-11-14 21:23:07 +0800

    链表

---
+++ Leetcode/链表题目/面试02.07.链表相交.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode *h1 = headA, *h2 = headB;
+        while (h1 != h2) {
+            h1 = h1 == NULL ? headB : h1->next;
+            h2 = h2 == NULL ? headA : h2->next;
+        }
+        return h1;
+    }
+};
