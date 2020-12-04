e6a460d42204a8a6eead14515a0d0533ca639092
4794-sourse <2456170006@qq.com>
2020-12-04 22:38:17 +0800

    2020.12.4

---
+++ Leetcode/算法/226. 翻转二叉树.cpp
@@ -0,0 +1,8 @@
+struct TreeNode* invertTree(struct TreeNode* root){
+    if (root == NULL) return NULL;
+    struct TreeNode *left = invertTree(root->left);
+    struct TreeNode *right = invertTree(root->right);
+    root->left = right;
+    root->right = left;
+    return root;
+}
