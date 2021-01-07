efa46928acac1740fef1de4b8ea9193ec6ed8aaf
4794-sourse <2456170006@qq.com>
2021-01-07 20:34:36 +0800

    2021.1.7

---
+++ Leetcode/算法/371. 两整数之和.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int getSum(int a, int b) {
+        while (b) {
+            int c = (unsigned int)(a & b) << 1;
+            a = a ^ b;
+            b = c;
+        }
+        return a;
+    }
+};
