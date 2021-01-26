455e2e8312e866118f9e4764f77aa473a9dbcd8c
4794-sourse <2456170006@qq.com>
2021-01-26 23:53:21 +0800

    2021.1.26

---
+++ Leetcode/算法/231. 2的幂.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if (n == 0) return false;
+        long long x = n;
+        return (x & (-x)) == n;
+    }
+};
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if (n == 0) return false;
+        long long x = n;
+        return (x & (x - 1)) == 0;
+    }
+};
